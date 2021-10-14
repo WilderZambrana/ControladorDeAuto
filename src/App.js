405dd2ff17b8d256caa9efae7c4f507546cacb50
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-10-14 05:51:13 -0400

    Coordenadas x,y de la posicion Inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 function posFinal(cadena){
-    const superficie = cadena.split(",");
-    if(superficie.length>1){
-        return superficie[0]+","+superficie[1]
+    const partesCadena = cadena.split("/");
+    //console.log(cadena.substr(2,1));
+    if(partesCadena.length>1){
+        const superficie = partesCadena[0].split(",");
+        const inicio = partesCadena[1].split(",");
+        if(superficie.length>1)
+            return Number(superficie[0])+","+Number(superficie[1])+"/"+Number(inicio[0])+","+Number(inicio[1])
+        return Number(superficie[0])+","+Number(superficie[1])+"/"+Number(inicio[0])+","+Number(inicio[1])
     }
-    return cadena+','+cadena;
+    else{
+        const superficie = cadena.split(",")
+        if(superficie.length>1)
+            return Number(superficie[0])+","+Number(superficie[1])
+        return Number(cadena)+','+Number(cadena);
+    }    
 }
+
+
 
 export default posFinal;
--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,14 @@ describe("Superficie de desplazamiento", () => {
     it("Deberia delvover la cadena 7,7 para una cadena que solo tiene la cadena 7", () => {
         expect(posFinal("7")).toEqual("7,7");
     });
-    it("Deberia delvover la cadena 4,7 para una cadena separada por coma 4,7", () => {
+    it("Deberia delvover la cadenas 4,7 para una cadena separada por coma 4,7", () => {
         expect(posFinal("4,7")).toEqual("4,7");
     });
-  });
+});
+
+describe("Posicion Inicial", () => {
+    it("Deberia delvover la cadena 1,3 para una cadena con superficie y posicicion inicial 5,6/1,3", () => {
+      expect(posFinal("5,6/1,3")).toEqual("5,6/1,3");
+    });
+
+});
